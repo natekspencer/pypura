0ac3e75ffeb188158cd48a0be6ee79592d35035a
Nathan Spencer <natekspencer@gmail.com>
2023-05-16 23:50:30 -0600

    Add fragrance codes

--- pypura/__init__.py
+++ pypura/__init__.py
@@ -1,6 +1,7 @@
 """pypura module."""
+from .const import FRAGRANCES
 from .exceptions import PuraApiException, PuraAuthenticationError
 from .pura import Pura
 
-__all__ = ["Pura", "PuraApiException", "PuraAuthenticationError"]
+__all__ = ["Pura", "PuraApiException", "PuraAuthenticationError", "FRAGRANCES"]
 __version__ = "0.1.0"
--- pypura/const.py
+++ pypura/const.py
@@ -1,3 +1,20 @@
 """Constants."""
+from typing import Final
+
 USER_POOL_ID = "dXMtZWFzdC0xX0xhQjcxOGhZdg=="
 CLIENT_ID = "NGlla3ViYXQwamI1aWxqZmJhYWxzaXFmOWo="
+
+FRAGRANCES: Final[dict[str, str]] = {
+    "AJV": "Birchwood Pine",
+    "BMC": "Balsam & Cedar",
+    "JRG": "Elsa Journeys to Ahtohallan",
+    "JZB": "Velvet Wineberry",
+    "JZJ": "Egyptian Sandalwood",
+    "KDR": "Ocean Mist & Sea Salt",
+    "KYV": "Tuscan Neroli",
+    "LHT": "Anna & Olaf in the Enchanted Forest",
+    "MBZ": "Pumpkin Chai",
+    "TWL": "Blue Water Lotus",
+    "WRT": "Charcoal",
+    "UDZ": "Bamboo",
+}
