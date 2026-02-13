ad3f04dfe5e4488c9278d913831911ed926dcbe5
Nathan Spencer <natekspencer@gmail.com>
2026-02-13 09:20:30 -0700

    Fix CI

--- scripts/update_license_year.sh
+++ scripts/update_license_year.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 YEAR=$(date +"%Y")
 
-# Works with: "Copyright (c) 2023", "Copyright © 2022–2024", etc.
-sed -i '' -E "s/(Copyright( \(c\)| ©)? [0-9]{4})(–[0-9]{4})?/\1–$YEAR/" LICENSE
+if sed --version >/dev/null 2>&1; then
+  # GNU sed
+  sed -i -E "s/(Copyright( \(c\)| ©)? [0-9]{4})(–[0-9]{4})?/\1–$YEAR/" LICENSE
+else
+  # BSD sed
+  sed -i '' -E "s/(Copyright( \(c\)| ©)? [0-9]{4})(–[0-9]{4})?/\1–$YEAR/" LICENSE
+fi
